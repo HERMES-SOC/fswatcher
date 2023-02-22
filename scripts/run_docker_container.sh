f608b69073cb17348736fa55eddef9b2fd8127a6
Damian Barrous-Dume <damianbarrous@gmail.com>
2023-02-22 16:33:24 +0000

    Fix variables

--- scripts/run_docker_container.sh
+++ scripts/run_docker_container.sh
@@ -39,45 +39,47 @@ docker build -t $IMAGE_NAME $DOCKERFILE_PATH
 echo "Running docker container $CONTAINER_NAME"
 
 # Unset all the environment variables
-unset $SDC_AWS_S3_BUCKET
-unset $SDC_AWS_CONCURRENCY_LIMIT
-unset $SDC_AWS_TIMESTREAM_DB
-unset $SDC_AWS_TIMESTREAM_TABLE
-unset $SDC_AWS_SLACK_TOKEN
-unset $SDC_AWS_SLACK_CHANNEL
+unset SDC_AWS_S3_BUCKET
+unset SDC_AWS_CONCURRENCY_LIMIT
+unset SDC_AWS_TIMESTREAM_DB
+unset SDC_AWS_TIMESTREAM_TABLE
+unset SDC_AWS_SLACK_TOKEN
+unset SDC_AWS_SLACK_CHANNEL
 
 # Docker environment variables
 SDC_AWS_S3_BUCKET="-b $S3_BUCKET_NAME"
 
 SDC_AWS_CONCURRENCY_LIMIT="-c $CONCURRENCY_LIMIT"
 
-# If TimeStream database name is not "", then add it to the environment variables
-if [ -z ${TIMESTREAM_DB+x} ]; then
+# If TimeStream database name is not "", then add it to the environment variables else make it empty
+if [ "$TIMESTREAM_DB" != "" ]; then
     SDC_AWS_TIMESTREAM_DB="-t $TIMESTREAM_DB"
 else
     SDC_AWS_TIMESTREAM_DB=""
 fi
 
-# If Timestream table name is not "", then add it to the environment variables
-if [ -z ${TIMESTREAM_TABLE+x} ]; then
+
+# If Timestream table name is not "", then add it to the environment variables else make it empty
+if [ "$TIMESTREAM_TABLE" != "" ]; then
     SDC_AWS_TIMESTREAM_TABLE="-tt $TIMESTREAM_TABLE"
 else
     SDC_AWS_TIMESTREAM_TABLE=""
 fi
 
 # If Slack token is not "", then add it to the environment variables else make it empty
-if [ -z ${SLACK_TOKEN+x} ]; then
+if [ "$SLACK_TOKEN" != "" ]; then
     SDC_AWS_SLACK_TOKEN="-s $SLACK_TOKEN"
 else
     SDC_AWS_SLACK_TOKEN=""
 fi
 
 # If Slack channel is not "", then add it to the environment variables else make it empty
-if [ -z ${SLACK_CHANNEL+x} ]; then
-    SDC_AWS_SLACK_CHANNEL="-c $SLACK_CHANNEL"
+if [ "$SLACK_CHANNEL" != "" ]; then
+    SDC_AWS_SLACK_CHANNEL="-sc $SLACK_CHANNEL"
 else
     SDC_AWS_SLACK_CHANNEL=""
 fi
+
 
 # Print all the environment variables
 echo "SDC_AWS_S3_BUCKET: $SDC_AWS_S3_BUCKET"
