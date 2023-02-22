aa8eb6beb989cede27d7e5559e154d5fca702fb0
Damian Barrous-Dume <damianbarrous@gmail.com>
2023-02-22 15:23:47 +0000

    Add optional env variables

--- scripts/run_docker_container.sh
+++ scripts/run_docker_container.sh
@@ -14,6 +14,24 @@ S3_BUCKET_NAME=swsoc-unsorted
 # Filepath to the directory to be watched
 WATCH_DIR=~/watch_directory
 
+# Concurrency limit
+CONCURRENCY_LIMIT=10
+
+# AWS region
+AWS_REGION="us-east-1"
+
+# TimeStream database name (optional)
+TIMESTREAM_DB=""
+
+# TimeStream table name (optional)
+TIMESTREAM_TABLE=""
+
+# Slack token (optional)
+SLACK_TOKEN=""
+
+# Slack channel (optional)
+SLACK_CHANNEL=""
+
 # Get path of current working directory (where the script is located)
 SCRIPT_PATH=$(pwd)
 
@@ -54,23 +72,40 @@ echo "Running docker container $CONTAINER_NAME"
 # Docker environment variables
 SDC_AWS_S3_BUCKET="-b $S3_BUCKET_NAME"
 
-SDC_AWS_CONCURRENCY_LIMIT='-c 10'
+SDC_AWS_CONCURRENCY_LIMIT="-c $CONCURRENCY_LIMIT"
 
-AWS_REGION='us-east-1'
+# If TimeStream database name is provided, then add it to the environment variables
+if [ ! -z "$TIMESTREAM_DB" ]; then
+    SDC_AWS_TIMESTREAM_DB="-t $TIMESTREAM_DB"
+fi
 
-echo "SDC_AWS_S3_BUCKET: $SDC_AWS_S3_BUCKET"
+# If TimeStream table name is provided, then add it to the environment variables
+if [ ! -z "$TIMESTREAM_TABLE" ]; then
+    SDC_AWS_TIMESTREAM_TABLE="-tt $TIMESTREAM_TABLE"
+fi
 
-echo "WATCH_DIR: $WATCH_DIR"
+# If Slack token is provided, then add it to the environment variables
+if [ ! -z "$SLACK_TOKEN" ]; then
+    SDC_AWS_SLACK_TOKEN="-s $SLACK_TOKEN"
+fi
 
-echo "AWS_REGION: $AWS_REGION"
+# If Slack channel is provided, then add it to the environment variables
+if [ ! -z "$SLACK_CHANNEL" ]; then
+    SDC_AWS_SLACK_CHANNEL="-sc $SLACK_CHANNEL"
+fi
 
+# Run the docker container
 docker run \
     -it \
     --name $CONTAINER_NAME \
     -e SDC_AWS_S3_BUCKET="$SDC_AWS_S3_BUCKET" \
     -e SDC_AWS_CONCURRENCY_LIMIT="$SDC_AWS_CONCURRENCY_LIMIT" \
+    -e SDC_AWS_TIMESTREAM_DB="$SDC_AWS_TIMESTREAM_DB" \
+    -e SDC_AWS_TIMESTREAM_TABLE="$SDC_AWS_TIMESTREAM_TABLE"
+    -e SDC_AWS_SLACK_TOKEN="$SDC_AWS_SLACK_TOKEN" \
+    -e SDC_AWS_SLACK_CHANNEL="$SDC_AWS_SLACK_CHANNEL" \
     -e AWS_REGION="$AWS_REGION" \
     -v /etc/passwd:/etc/passwd \
     -v $WATCH_DIR:/watch \
     -v ${HOME}/.aws/credentials:/root/.aws/credentials:ro \
-    $IMAGE_NAME 
+    $IMAGE_NAME
