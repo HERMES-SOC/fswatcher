835d12bc89c896e0cc8695413d99e5ccc8c0ffec
Damian Barrous-Dume <damianbarrous@gmail.com>
2023-04-06 18:10:56 +0100

    Integrate to use aws_sdc_utils for slack and timestream

--- fswatcher/__main__.py
+++ fswatcher/__main__.py
@@ -1,22 +1,13 @@
-"""
-Main File for the AWS File System Watcher
-"""
 import sys
 import time
-import logging
-from watchdog.observers.polling import PollingObserver
+
+from watchdog.observers import Observer
+from aws_sdc_utils.logging import log
+
 from fswatcher.FileSystemHandler import FileSystemHandler
 from fswatcher.FileSystemHandlerConfig import get_config
 
-# Configure logging
-logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s %(message)s",
-    level=logging.INFO,
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
 
-
-# Main Function
 def main() -> None:
     """
     Main Function
@@ -28,47 +19,42 @@ def main() -> None:
     # Initialize the FileSystemHandler
     event_handler = FileSystemHandler(config=config)
 
-    # Try to use the inotify observer
+    # Initialize the Observer and start watching
+    log.info("Starting observer")
+    observer = Observer()
+
     try:
-        # Initialize the Observer and start watching
-        logging.info("Starting observer")
-        observer = PollingObserver()
         observer.schedule(event_handler, config.path, recursive=True)
-        print(observer.event_queue.maxsize)
         observer.start()
+
         # If backtrack is enabled, run the initial scan
         if config.backtrack:
-            logging.info(
+            log.info(
                 "Backtracking enabled, backtracking (This might take awhile if a large amount of directories and files)..."
             )
             event_handler.backtrack(
                 config.path, event_handler.parse_datetime(config.backtrack_date)
             )
-            logging.info("Backtracking complete")
+            log.info("Backtracking complete")
             config.backtrack = False
-        logging.info(
-            f"Watching for file events with INotify Observer in: {config.path}"
-        )
 
+        log.info(f"Watching for file events with INotify Observer in: {config.path}")
+
+        while True:
+            time.sleep(1)
     except OSError:
-        # If inotify fails, use the polling observer
-        logging.warning("INotify Limit Reached, falling back to polling observer.")
-        logging.warning(
+        log.warning("INotify Limit Reached, falling back to polling observer.")
+        log.warning(
             "We suggest you increase the inotify limit for better performance, see: https://gist.github.com/coenraadhuman/fa7345e95a9b4dea851dbe9e8f011470"
         )
-        logging.warning(
+        log.warning(
             "This is limited by your RAM, 1,000,000 Directory Watches per 1GB of RAM, see: https://unix.stackexchange.com/questions/13751/kernel-inotify-watch-limit-reached"
         )
         sys.exit(0)
-
-    try:
-        while True:
-            time.sleep(1)
     finally:
         observer.stop()
         observer.join()
 
 
-# Main Function
 if __name__ == "__main__":
     main()
