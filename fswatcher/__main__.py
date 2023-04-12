984bc3a13623f7081c69022125f72891268f97a3
Damian Barrous-Dume <damianbarrous@gmail.com>
2023-04-12 17:53:24 +0100

    test

--- fswatcher/__main__.py
+++ fswatcher/__main__.py
@@ -1,16 +1,3 @@
-import sys
-import time
-
-from watchdog.observers import Observer
-
-from fswatcher import log
-from fswatcher.FileSystemHandler import FileSystemHandler
-from fswatcher.FileSystemHandlerConfig import get_config
-
-# Change the log level to info
-log.setLevel("INFO")
-
-
 import os
 import time
 import sqlite3
@@ -101,7 +88,7 @@ def process_files(conn, all_files):
 
 
 def main():
-    path = "/watch"
+    path = "./watch"
     max_workers = 1
     check_interval = 0
 
@@ -117,7 +104,7 @@ def main():
 
     conn = init_db()
 
-    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
         # while True:
         start = time.time()
         time.sleep(
@@ -143,66 +130,14 @@ def main():
         # Check for new, updated, and deleted files
         new_files, deleted_files = process_files(conn, all_files)
 
-        # if new_files:
-        #     print(f"New or updated files found: {new_files}")
-        # if deleted_files:
-        #     print(f"Deleted files found: {deleted_files}")
-        # Print the size in bytes of the database
-        print(f"Database size: {os.path.getsize('file_info.db')} bytes")
+        if new_files:
+            print(f"New or updated files found: {new_files}")
+        if deleted_files:
+            print(f"Deleted files found: {deleted_files}")
+
         end = time.time()
         print(f"Time taken: {end - start} seconds")
 
 
 if __name__ == "__main__":
     main()
-
-# def main() -> None:
-#     """
-#     Main Function
-#     """
-
-#     # Get the configuration dataclass object
-#     config = get_config()
-
-#     # Initialize the FileSystemHandler
-#     event_handler = FileSystemHandler(config=config)
-
-#     # Initialize the Observer and start watching
-#     log.info("Starting observer")
-#     observer = Observer()
-
-#     try:
-#         observer.schedule(event_handler, config.path, recursive=True)
-#         observer.start()
-
-#         # If backtrack is enabled, run the initial scan
-#         if config.backtrack:
-#             log.info(
-#                 "Backtracking enabled, backtracking (This might take awhile if a large amount of directories and files)..."
-#             )
-#             event_handler.backtrack(
-#                 config.path, event_handler.parse_datetime(config.backtrack_date)
-#             )
-#             log.info("Backtracking complete")
-#             config.backtrack = False
-
-#         log.info(f"Watching for file events with INotify Observer in: {config.path}")
-
-#         while True:
-#             time.sleep(1)
-#     except OSError:
-#         log.warning("INotify Limit Reached, falling back to polling observer.")
-#         log.warning(
-#             "We suggest you increase the inotify limit for better performance, see: https://gist.github.com/coenraadhuman/fa7345e95a9b4dea851dbe9e8f011470"
-#         )
-#         log.warning(
-#             "This is limited by your RAM, 1,000,000 Directory Watches per 1GB of RAM, see: https://unix.stackexchange.com/questions/13751/kernel-inotify-watch-limit-reached"
-#         )
-#         sys.exit(0)
-#     finally:
-#         observer.stop()
-#         observer.join()
-
-
-# if __name__ == "__main__":
-#     main()
